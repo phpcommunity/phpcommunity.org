3938247d23abacb0e84f456560c75ce8a215f056
Ben Ramsey <ben@benramsey.com>
2013-11-18 14:55:50 -0600

    Add symbolic link to composer.phar

--- .puppet/modules/setup/manifests/php/composer.pp
+++ .puppet/modules/setup/manifests/php/composer.pp
@@ -5,4 +5,11 @@ class setup::php::composer {
         command => "curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin",
     }
 
+    file { "/usr/local/bin/composer":
+        ensure  => "link",
+        path    => "/usr/local/bin/composer",
+        target  => "/usr/local/bin/composer.phar",
+        require => Exec['Download and install Composer'],
+    }
+
 }
