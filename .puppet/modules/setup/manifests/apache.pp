0e1dd0d3f01665283fc8bc3af16aeee7586ca71c
Ben Ramsey <ben@benramsey.com>
2013-12-07 10:30:02 -0600

    Remove unnecessary Apache modules

--- .puppet/modules/setup/manifests/apache.pp
+++ .puppet/modules/setup/manifests/apache.pp
@@ -1,36 +1,6 @@
 # Set up Apache modules
 class setup::apache {
 
-    file { "proxy.conf":
-        ensure  => "link",
-        path    => "/etc/apache2/mods-enabled/proxy.conf",
-        target  => "/etc/apache2/mods-available/proxy.conf",
-    }
-
-    file { "proxy.load":
-        ensure  => "link",
-        path    => "/etc/apache2/mods-enabled/proxy.load",
-        target  => "/etc/apache2/mods-available/proxy.load",
-    }
-
-    file { "proxy_http.load":
-        ensure  => "link",
-        path    => "/etc/apache2/mods-enabled/proxy_http.load",
-        target  => "/etc/apache2/mods-available/proxy_http.load",
-    }
-
-    file { "userdir.conf":
-        ensure  => "link",
-        path    => "/etc/apache2/mods-enabled/userdir.conf",
-        target  => "/etc/apache2/mods-available/userdir.conf",
-    }
-
-    file { "userdir.load":
-        ensure  => "link",
-        path    => "/etc/apache2/mods-enabled/userdir.load",
-        target  => "/etc/apache2/mods-available/userdir.load",
-    }
-
     file { "rewrite.load":
         ensure  => "link",
         path    => "/etc/apache2/mods-enabled/rewrite.load",
